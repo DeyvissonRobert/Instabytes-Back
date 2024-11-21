76b9b2b826add74b902f0fbda8e1651806f4b60b
DeyvissonRobert <robert.designerx@gmail.com>
2024-11-20 22:58:47 -0300

    Modificando Rotas

--- server.js
+++ server.js
@@ -1,30 +1,9 @@
 import express from "express";
 
 const posts = [
-    {
-        descricao: "Foto Teste",
-        imagem: "https://placecats.com/millie/300/250",
-    },
-    {
-        descricao: "Gato brincando com um novelo de lã",
-        imagem: "https://picsum.photos/id/237/300/250",
-    },
-    {
-        descricao: "Paisagem com um gato",
-        imagem: "https://source.unsplash.com/random/300x250/?cat,landscape",
-    },
-    {
-        descricao: "Gato dormindo em uma caixa",
-        imagem: "https://placekitten.com/300/250",
-    },
-    {
-        descricao: "Gato olhando pela janela",
-        imagem: "https://unsplash.com/photos/300x250/?query=cats&orientation=landscape",
-    },
-    {
-        descricao: "Gato comendo ração",
-        imagem: "https://placekitten.com/g/300/250",
-    },
+    { descricao: 'Foto Teste', imagem: 'https://picsum.photos/id/237/300/250', id: 1 },
+    { descricao: 'Paisagem com um gato', imagem: 'https://picsum.photos/id/237/300/250', id: 2 },
+    { descricao: 'Gato brincando com um novelo de lã', imagem: 'https://picsum.photos/id/237/300/250', id: 3 },
 ];
 
 const app = express()
