0e55c3d34cf28938d1e9b5ed9f778741667ef51c
DeyvissonRobert <robert.designerx@gmail.com>
2024-11-24 20:05:46 -0300

    Criando Rota get/:id

--- server.js
+++ server.js
@@ -16,3 +16,14 @@ app.listen(3000, () => {
 app.get("/posts", (req, res) => {
     res.status(200).json(posts);  
 });
+
+function buscarPostPorID(id) {
+    return posts.findIndex((post) => {
+        return post.id === Number(id)
+    })
+}
+
+app.get("/posts/:id", (req, res) => {
+    const index = buscarPostPorID(req.params.id)
+    res.status(200).json(posts[index]);  
+});
