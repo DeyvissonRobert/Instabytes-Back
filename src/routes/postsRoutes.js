c84ee70029cee5eeb90c847f685a21985dac357c
DeyvissonRobert <robert.designerx@gmail.com>
2024-12-19 21:17:22 -0300

    ADD comentários

--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,30 +1,21 @@
-import express from "express";
-import multer from "multer";
-import { listarPosts, postarNovoPost, uploadImagem } from "../controllers/postsController.js";
+import express from "express"; // Importa o framework Express para criar a aplicação.
+import multer from "multer"; // Importa o módulo Multer para lidar com uploads de arquivos.
+import { listarPosts, postarNovoPost, uploadImagem } from "../controllers/postsController.js"; // Importa funções para controlar as ações dos posts.
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'uploads/');
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.originalname);
-    }
+const storage = multer.diskStorage({ // Configura como os arquivos serão armazenados.
+  destination: (req, file, cb) => cb(null, 'uploads/'), // Define a pasta de destino para os uploads.
+  filename: (req, file, cb) => cb(null, file.originalname) // Define o nome do arquivo.
 });
 
-const upload = multer({ dest: "./uploads", storage })
+const upload = multer({ storage }); // Cria uma instância do Multer com as configurações de armazenamento.
 //Linux ou no mac
 //const upload = multer({ dest: './uploads'});
 
-
 const routes = (app) => {
-    app.use(express.json());
-    // Permite que o servidor interprete requisições com corpo em formato JSON.
-    app.get("/posts", listarPosts);
-    //Rota para buscar todos os posts
-    app.post("/posts", postarNovoPost)
-    //Rotaa para criar um post
-    app.post("/upload", upload.single("imagem"), uploadImagem)
-    
-}
+  app.use(express.json()); // Permite lidar com dados JSON nas requisições.
+  app.get("/posts", listarPosts); // Rota para listar todos os posts.
+  app.post("/posts", postarNovoPost); // Rota para criar um novo post.
+  app.post("/upload", upload.single("imagem"), uploadImagem); // Rota para fazer upload de uma imagem.
+};
 
-export default routes;
+export default routes; // Exporta a função das rotas para ser usada em outro arquivo.
